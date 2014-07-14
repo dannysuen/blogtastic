006e6def4b70f906586a9199f0b4d20bd4b7b3bb
Danny Suen <sunnyhust2005@qq.com>
2014-07-14 15:02:08 +0800

    1. Add more blog test data for load.sql; 2. Add comments for blog.

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
 require("header.php");
 
-$sql = "select Entries.*, Categories.CategoryName from Entries, Categories where Entries.CategoryID = Categories.CategoryID order by DatePosted desc limit 2";
+$sql = "select Entries.*, Categories.CategoryName from Entries, Categories where Entries.CategoryID = Categories.CategoryID order by DatePosted desc limit 1";
 
 
 $db_query = $db_handler->query($sql);
 
 $db_query2 = $db_handler->query($sql);
-$db_query2->setFetchMode(PDO::FETCH_ASSOC);
-while ($row = $db_query2->fetch())
 
 $latest_row = $db_query->fetch(PDO::FETCH_OBJ);
 $second_latest_row = $db_query->fetch(PDO::FETCH_OBJ);
 
 ?>
-
 
     <div class="container">
 
@@ -27,21 +24,34 @@ $second_latest_row = $db_query->fetch(PDO::FETCH_OBJ);
 
         $comments_sql = "select Name from Comments where BlogID = " . $latest_row->EntryID . " order by DatePosted;";
         $comments_result = $db_handler->query($comments_sql);
-        $comments_rows_count = $comments_result->fetchColumn();
+        $comments_rows_count = count($comments_result->fetchAll());
         if ($comments_rows_count == 0) {
-          echo "<p class='lead'>No comments</p>";
+          echo "<p class='lead'>No comments.</p>";
         } else {
-          echo "<p class='lead'>(" . $comments_rows_count . ") comments.</p>";
+          $comments_result2 = $db_handler->query($comments_sql);
+          echo "<p class='lead'>(<strong>" . $comments_rows_count . "</strong>) comments : ";
+          $i = 1;
+          while ($comment_row = $comments_result2->fetch(PDO::FETCH_ASSOC)) {
+            echo "<a href='viewentry.php?id=" . $latest_row->EntryID . "#comment" . $i . "'>" . $comment_row['Name'] . "</a> ";
+            $i++;
+          }
+          echo "</p>";
+        }
+
+        $prevsql = "select Entries.*, Categories.CategoryName from Entries, Categories where Entries.CategoryID = Categories.CategoryID order by DatePosted desc limit 1, 5;";
+        $prev_numrows = count($db_handler->query($prevsql)->fetchAll());
+        if ($prev_numrows == 0) {
+          echo "<p class='lead'>No previous entries.</p>";
+        } else {
+          echo "<ul class='lead'>";
+          $prev_result = $db_handler->query($prevsql);
+          while ($prev_row = $prev_result->fetch(PDO::FETCH_OBJ)) {
+            echo "<li><a href='viewentry.php?id=" . $prev_row->EntryID . "'>" . $prev_row->Subject . "</a></li>";
+          }
+          echo "</ul>";
         }
 
         ?></p>
-      </div>
-
-      <div class="starter-template">
-        <h1><?php echo "<a href='viewentry.php?id=" . $second_latest_row->EntryID . "'>" . $second_latest_row->Subject . "</a>"; ?></h1>
-        <p class="lead"><?php echo "<i>In <a href='viewentry.php?id=" . $second_latest_row->CategoryID . "'>"
-        . $second_latest_row->CategoryName . "</a> - Posted on " . date("F jS Y", strtotime($second_latest_row->DatePosted)) . "</i>"; ?></p>
-        <p class="lead"><?php echo nl2br($second_latest_row->Body); ?></p>
       </div>
 
 
