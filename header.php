65475f94b5a256f66a3c9fc97216ac0fe5bd32ca
Danny Suen <sunnyhust2005@qq.com>
2014-07-21 11:44:06 +0800

    removing sign-in form in header.

--- header.php
+++ header.php
@@ -79,15 +79,6 @@ try {
             <?php endif ?>
 
           </ul>
-          <form class="navbar-form navbar-right" role="form">
-            <div class="form-group">
-              <input type="text" placeholder="User name" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
-          </form>
         </div><!--/.navbar-collapse -->
       </div>
     </div>
--- updateentry_header.php
+++ updateentry_header.php
@@ -66,16 +66,6 @@
             <?php endif ?>
           </ul>
 
-          <form class="navbar-form navbar-right" role="form">
-            <div class="form-group">
-              <input type="text" placeholder="User name" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
-          </form>
-
         </div><!--/.navbar-collapse -->
       </div>
     </div>
